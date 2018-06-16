27ad1a33d0bc04938b10e9834adf44be5512b807
Prabhat Ojha <mailprabhat98@gmail.com>
2018-06-16 13:40:29 +0530

    creating npm packages and using them

---
+++ Sample_npm_package/Another_npm_package/index.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+  key1:"Prabhat98",
+  key2:1234,
+  key:true
+}
---
+++ Sample_npm_package/index.js
@@ -0,0 +1,5 @@
+
+
+let firstPackage = require("./Another_npm_package")// required the entire package but the data we got only from index.js file
+
+console.log(firstPackage);
